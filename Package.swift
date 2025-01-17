7b43410a94264b0ef43396d0165028f00554561a
Rocket Jerry <jerry.hangyu@gmail.com>
2025-01-17 19:40:55 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,12 +10,14 @@ let package = Package(
         .library(name: "OpenIMSDK", targets: ["OpenIMSDK"])
     ],
     dependencies: [
+        .package(url: "https://github.com/bc1pjerry/open-im-sdk-ios.git", from: "1.0.1"),
         .package(url: "https://github.com/CoderMJLee/MJExtension.git", from: "3.0.0"),
     ],
     targets: [
         .target(
             name: "OpenIMSDK",
             dependencies: [
+                "OpenIMSDK",
                 "MJExtension"
             ],
             path: "Sources",
